2d20d9730fac2df3c78415445437476414672e96
Benjamin Perry* <famogr.perry@gmail.com>
2024-05-08 10:37:34 -0400

    Added Battle Scene & Transition to battle scene in areas with Grass

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int speed;
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer playerSprite;
+    [SerializeField] private LayerMask grassLayer;
+    [SerializeField] private float timeBetweenBattles = 10f;
 
     private PlayerControls playerControls;
     private Rigidbody rb;
     private Vector3 movement;
+    private bool movingInBattleArea;
+    private float battleCountdown;
 
     private const string IS_WALK_PARAM = "IsWalking";
+    private const string PLAINS_BATTLE_SCENE = "Battle_Plains_Scene";
 
     private void Awake() {
         playerControls = new PlayerControls();
+        battleCountdown = Random.Range(timeBetweenBattles * 0.7f, timeBetweenBattles * 1.3f);
     }
 
     private void OnEnable() {
@@ -47,5 +54,17 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate() {
         rb.MovePosition(transform.position + movement * speed * Time.fixedDeltaTime);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 1, grassLayer);
+        movingInBattleArea = colliders.Length != 0 && movement != Vector3.zero;
+
+        if (movingInBattleArea) {
+            battleCountdown -= Time.deltaTime;
+
+            if (battleCountdown <= 0) {
+                battleCountdown = Random.Range(timeBetweenBattles * 0.7f, timeBetweenBattles * 1.3f);
+                SceneManager.LoadScene(PLAINS_BATTLE_SCENE);
+            }
+        }
     }
 }
